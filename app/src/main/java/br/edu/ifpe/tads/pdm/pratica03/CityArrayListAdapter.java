350a532f082f0bb6fa832b8066af1d53a89cbff5
dumpserver <dumpserver@hotmail.com>
2022-02-09 20:38:32 -0300

    Include holder when mount cityName and cityInfo TextView

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityArrayListAdapter.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica03/CityArrayListAdapter.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 public class CityArrayListAdapter extends ArrayAdapter<City> {
     private City [] cities;
 
+    static class ViewHolder {
+        TextView cityName;
+        TextView cityInfo;
+    }
+
     public CityArrayListAdapter(Context context, int resource, City [] cities) {
         super(context, resource, cities);
         this.cities = cities;
@@ -17,12 +22,26 @@ public class CityArrayListAdapter extends ArrayAdapter<City> {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View listItem = inflater.inflate(R.layout.city_listitem, null, true);
-        TextView cityName = (TextView) listItem.findViewById(R.id.city_name);
-        TextView cityInfo = (TextView) listItem.findViewById(R.id.city_info);
-        cityName.setText(cities[position].getName());
-        cityInfo.setText(cities[position].getInfo());
+        View listItem = null;
+
+        ViewHolder holder = null;
+
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+
+            listItem = inflater.inflate(R.layout.city_listitem, null, true);
+
+            holder = new ViewHolder();
+            holder.cityName = (TextView) listItem.findViewById(R.id.city_name);
+            holder.cityInfo = (TextView) listItem.findViewById(R.id.city_info);
+            listItem.setTag(holder);
+        } else {
+            listItem = view;
+            holder = (ViewHolder)view.getTag();
+        }
+
+        holder.cityName.setText(cities[position].getName());
+        holder.cityInfo.setText(cities[position].getInfo());
         return listItem;
     }
 }
